f198802b326e7c45cbe9d36b1861b624bb35798b
Herbert Gabriel <herbertgacruz@gmail.com>
2025-01-10 01:14:39 -0300

    adiciona filtro de segurança personalizado e ajusta configuração de autenticação; renomeia tabela de pacientes para usuários no banco de dados

--- src/main/resources/db/migration/V03__create-table-usuarios.sql
+++ src/main/resources/db/migration/V03__create-table-usuarios.sql
@@ -1,4 +1,4 @@
-create table if not exists pacientes(
+create table if not exists usuarios(
     id bigint not null auto_increment,
     login varchar(100) not null unique,
     senha varchar(255) not null,
